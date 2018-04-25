e7cfe7cf5675e455ca6e78f087f1554c6375bc1c
FominAO <afomin97@mail.ru>
2018-04-26 02:53:20 +0300

    fixsed slide size

--- js/slide.js
+++ js/slide.js
@@ -19,7 +19,7 @@ function Slider() {
         for (i = 0; i < slides.length; i++) {
             slides[i].style.display = "none";
         }
-        slides[this.slideIndex - 1].style.display = "block";
+        slides[this.slideIndex - 1].style.display = "flex";
     }
 }
 slider = new Slider();
