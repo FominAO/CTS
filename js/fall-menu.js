62910f45e37b8f97a3d710e964c57236e76b76de
FominAO <afomin97@mail.ru>
2018-04-28 16:05:52 +0300

    save

---
+++ js/fall-menu.js
@@ -0,0 +1,12 @@
+function fall() {
+    var x = document.getElementsByClassName("fall-menu");
+    var over = getComputedStyle(x[0]);
+    var y = over.overflow;
+    x[0].style.top = "70px"; 
+    // var y = document.getElementsByClassName("menu-point");
+    // for (let i = 0; i < y.length; i++) {
+    //     y[i].style.display = "block";
+    // }
+    
+     }
+    
